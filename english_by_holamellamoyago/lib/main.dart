bf239a8b6723f5e342372b6bc1f5ea0ddfb5ed18
Yago Otero <yagootero2003@gmail.com>
2024-12-22 14:58:25 +0100

    Añadida la supabase con dotenv

--- english_by_holamellamoyago/lib/main.dart
+++ english_by_holamellamoyago/lib/main.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:english_by_holamellamoyago/config/constants/Environment.dart';
 import 'package:english_by_holamellamoyago/presentation/screens.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -10,8 +11,8 @@ void main() async {
   await dotenv.load(fileName: ".env");
 
   await Supabase.initialize(
-    url: 'https://ohnbdtecylptrfjfsmvv.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9obmJkdGVjeWxwdHJmamZzbXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5MjM2OTQsImV4cCI6MjA0ODQ5OTY5NH0.xQ1d_ne3ugHlXMG8WhM0DU_OiG_rAxIoyxiIg7Gm6ZQ',
+    url: environment.supabase_url,
+    anonKey: environment.supabase_key,
   );
 
   runApp(const MyApp());

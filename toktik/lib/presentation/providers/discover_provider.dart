b0edc81e2b0744f8475d09f1ffbb5a2b615ab451
Yago Otero <Yagootero2003@gmail.com>
2025-08-21 04:40:06 +0200

    Termianda de entender un poco mas la arquitectura limpia, implementados los videos con buena arquitectura

--- toktik/lib/presentation/providers/discover_provider.dart
+++ toktik/lib/presentation/providers/discover_provider.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 
 import 'package:toktik/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-  // TODO: Repository, DataSource
-  final VideoPostR videoRepositort;
+  //  Repository, DataSource
+  final VideoPostsRepositoryImpl videoRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
+  // Constructor
+  DiscoverProvider({required this.videoRepository});
+
   Future<void> loadNextPage() async {
     // await Future.delayed( const Duration(seconds: 2) );
 
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-        .toList();
+    // final List<VideoPost> newVideos = videoPosts
+    //     .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //     .toList();
+
+    final newVideos = await videoRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;

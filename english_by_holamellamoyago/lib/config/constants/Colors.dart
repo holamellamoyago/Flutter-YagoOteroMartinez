3ce19c3adb790eda0a1112f9d985bc73cf2587ae
Yago Otero <Yagootero2003@gmail.com>
2025-09-14 16:06:00 +0200

    Quitadas aplicaciones vacias

--- english_by_holamellamoyago/lib/config/constants/Colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:english_by_holamellamoyago/presentation/screens.dart';
-
-class ColorsCustom{
-  // TODO Cambiar todos los colores hasta ahora
-  static final primario = Colors.grey[300];
-  final background = Colors.blue[200];
-}

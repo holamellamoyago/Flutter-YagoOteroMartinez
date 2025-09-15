6b11e983157ac11c38de7175803b0924c2cd04a3
Yago Otero <Yagootero2003@gmail.com>
2025-09-15 22:58:01 +0200

    Antes de implementar Clean code

--- provider_mvvm_app/lib/view/list_users.dart
+++ provider_mvvm_app/lib/view/list_users.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_mvvm_app/models/user.dart';
 import 'package:provider_mvvm_app/viewmodel/users_viewmodel.dart';
 
 class UsersList extends StatelessWidget {
@@ -8,16 +7,8 @@ class UsersList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // UsersViewmodel provider = Provider.of<UsersViewmodel>(context);
     final provider = context.watch<UsersViewmodel>();
     
-    if (provider.users.isEmpty) {
-      return ElevatedButton(
-        onPressed: provider.loadUsers,
-        child: Text("Recargar"),
-      );
-    }
-
     return provider.loading
         ? CircularProgressIndicator()
         : provider.error != null

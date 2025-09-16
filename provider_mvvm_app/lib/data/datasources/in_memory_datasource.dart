f51351d5165bdf18b5ee42c299d93c6f57b2c44c
Yago Otero <Yagootero2003@gmail.com>
2025-09-16 21:33:14 +0200

    Añadidas estructura de MVVM + Clean Architecture

---
+++ provider_mvvm_app/lib/data/datasources/in_memory_datasource.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider_mvvm_app/data/datasources/local_user_datasource.dart';
+import 'package:provider_mvvm_app/domain/entities/user.dart';
+
+class InMemoryDatasource implements LocalUserDatasource {
+
+  final List<User> _storage = [];
+
+  @override
+  Future<void> addUser(User user) async {
+    _storage.add(user);
+  }
+
+  @override
+  Future<List<User>> getUsers() async {
+    User u1 = User("holamellamoyago");
+    User u2 = User("pepe222");
+    User u3 = User("menganito");
+
+    _storage.add(u1);
+    _storage.add(u2);
+    _storage.add(u3);
+
+    return List.unmodifiable(_storage);
+  }
+  
+}

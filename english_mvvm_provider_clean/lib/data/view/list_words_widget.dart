c8f225a83330a431b21ddf659f0168d931c82999
Yago Otero <Yagootero2003@gmail.com>
2025-09-18 23:05:00 +0200

    Importado el paquete de carrousel Slider

--- english_mvvm_provider_clean/lib/data/view/list_words_widget.dart
+++ english_mvvm_provider_clean/lib/data/view/list_words_widget.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:english_mvvm_provider_clean/data/viewmodel/words_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,12 +10,26 @@ class ListWordsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = context.watch<WordsViewModel>();
 
+    // return Expanded(
+    //   child: ListView.builder(
+    //     scrollDirection: Axis.horizontal,
+    //     itemCount: provider.words!.length,
+    //     itemBuilder: (context, index) =>
+    //         Text(provider.words!.elementAt(index).english),
+    //   ),
+    // );
+
     return Expanded(
-      child: ListView.builder(
+      child: CarouselSlider.builder(
         itemCount: provider.words!.length,
-        itemBuilder: (context, index) =>
-            Text(provider.words!.elementAt(index).english),
+        itemBuilder: (context, index, realIndex) =>
+            Text(provider.words!.elementAt(index).spanish),
+        options: CarouselOptions(),
       ),
     );
   }
+  
 }
+
+
+

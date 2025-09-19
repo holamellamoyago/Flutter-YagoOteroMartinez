55c3a818090be2b72a45fb03d703ec4618ceb033
Yago Otero <Yagootero2003@gmail.com>
2025-09-19 18:46:02 +0200

    Añadidas distintas fuentes y estilos de fuientes junto con su provider

---
+++ english_mvvm_provider_clean/lib/data/viewmodel/themedata_viewmodel.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemedataViewmodel extends ChangeNotifier {
+  bool isLight = true;
+
+  ThemeData get() => isLight ? themeDataLight : ThemeData.dark();
+
+  ThemeData themeDataLight = ThemeData(
+    fontFamily: "Comic Relief",
+    textTheme: TextTheme(
+      headlineLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      bodyMedium: TextStyle(
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}

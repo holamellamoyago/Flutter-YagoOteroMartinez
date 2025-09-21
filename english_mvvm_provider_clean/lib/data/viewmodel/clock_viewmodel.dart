ef548ff2e1babcce4efd50e70c54301b67551a3e
Yago Otero <Yagootero2003@gmail.com>
2025-09-21 15:36:55 +0200

    Terminado el provider del Timer

--- english_mvvm_provider_clean/lib/data/viewmodel/clock_viewmodel.dart
+++ english_mvvm_provider_clean/lib/data/viewmodel/clock_viewmodel.dart
@@ -1,8 +1,30 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 
 class ClockViewmodel extends ChangeNotifier {
-  Color color = Colors.green;
-  int segundos = 10;
+  CountDownController countDownController = CountDownController();
+  Color fillColor = Colors.green;
+  Color ringColor = Colors.greenAccent;
 
-  
+  final int segundos = 10;
+
+  void onChangeCountdowm(String? value) {
+    var num = int.parse(value!);
+
+    switch (num) {
+      case < 4:
+        fillColor = Colors.red;
+        ringColor = Colors.orangeAccent;
+        break;
+      case < 6:
+        fillColor = Colors.orange;
+        ringColor = Colors.lime;
+        break;
+      default:
+        fillColor = Colors.green;
+        ringColor = Colors.greenAccent;
+    }
+
+    Future.microtask(() => notifyListeners());
+  }
 }

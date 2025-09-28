815f2efb3debc9bac59fc5ef90f63c397a3ab764
Yago Otero <yagootero2003@gmail.com>
2025-09-28 09:50:54 +0200

    Implementado flutter_dotenv

---
+++ english_mvvm_provider_clean/lib/config/app_env.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppEnv {
+  static final String webApiKey = dotenv.env['WEB_apiKey']!;
+  static final String androidApiKey = dotenv.env['Android_apiKey']!;
+  static final String iosApiKey = dotenv.env['IOS_apiKey']!;
+  static final String macosApiKey = dotenv.env['MacOS_apiKey']!;
+  static final String windowsApiKey = dotenv.env['Windows_apiKey']!;
+}
---
+++ english_mvvm_provider_clean/lib/data/view/loggin_screen/loggin_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LogginScreen extends StatelessWidget {
+  const LogginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Phrasal Quest"), centerTitle: true,),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+      ),
+    );
+  }
+}

34b843d94936c611954e15997aab4e4f8f1f015b
Yago Otero <yagootero2003@gmail.com>
2025-10-14 10:43:05 +0200

    Mejorada la UI de social_screen 2

--- english_mvvm_provider_clean/lib/domain/entities/app_user.dart
+++ english_mvvm_provider_clean/lib/domain/entities/app_user.dart
@@ -1,12 +1,18 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
-import 'package:flutter/widgets.dart';
-
 class AppUser {
-  String name, email;
-  String? image;
+  String name, email, createdAt;
+  String? image, username;
+  int totalPoints;
 
-  AppUser({required this.name, required this.email, this.image});
+  AppUser({
+    required this.name,
+    required this.email,
+    required this.totalPoints,
+    required this.createdAt,
+    this.username,
+    this.image,
+  });
 
   @override
   String toString() => "$name($email) imagen: $image";

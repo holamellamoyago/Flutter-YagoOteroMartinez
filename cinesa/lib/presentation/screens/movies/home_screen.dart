064dfb86a2c22b73e06b40db1f840313352b9617
Yago Otero <yagootero2003@gmail.com>
2026-01-16 19:04:08 +0100

    Aplicación mostrando correctamente las peliculas de la home screen

--- cinesa/lib/presentation/screens/movies/home_screen.dart
+++ cinesa/lib/presentation/screens/movies/home_screen.dart
@@ -40,72 +40,75 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
     final upComingMovies = ref.watch(upComingMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
     final moviesSlideshowProvider = ref.watch(moviesSlideShowProvider);
 
-    return FullScreenLoader();
-
-    return moviesSlideshowProvider.isEmpty
-        ? Center(child: CircularProgressIndicator())
-        : CustomScrollView(
-            slivers: [
-              SliverAppBar(floating: true, flexibleSpace: CustomAppbar()),
-              SliverList(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  return Column(
-                    children: [
-                      MoviesSlideshow(movies: moviesSlideshowProvider),
-                      MovieHorizontalListview(
-                        movies: nowPlayingMovies,
-                        title: "En cines",
-                        subtitle: "Sábado 27",
-                        loadNextPage: () {
-                          ref
-                              .read(nowPlayingMoviesProvider.notifier)
-                              .loadNextPage();
-                        },
-                      ),
-                      // Proximamente
-                      MovieHorizontalListview(
-                        movies: upComingMovies,
-                        title: "Proximamente",
-                        loadNextPage: () {
-                          ref
-                              .read(upComingMoviesProvider.notifier)
-                              .loadNextPage();
-                        },
-                      ),
-                      // Peliculas populares
-                      MovieHorizontalListview(
-                        movies: popularMovies,
-                        title: "Populares",
-                        subtitle: "En este mes",
-                        loadNextPage: () {
-                          ref
-                              .read(popularMoviesProvider.notifier)
-                              .loadNextPage();
-                        },
-                      ),
-                      MovieHorizontalListview(
-                        movies: topRatedMovies,
-                        title: "Mejor calificadas",
-                        subtitle: "Desde siempre",
-                        loadNextPage: () {
-                          ref
-                              .read(topRatedMoviesProvider.notifier)
-                              .loadNextPage();
-                        },
-                      ),
-                      SizedBox(height: 8.h),
-                    ],
-                  );
-                }, childCount: 1),
-              ),
-            ],
-          );
+    return initialLoading
+        ? FullScreenLoader()
+        // Lo que hace este widget es quitar la visibilidad para que se vayan cargando las imagenes pero en cambio no se mire el widget
+        : Visibility(
+          visible: !initialLoading,
+          child: CustomScrollView(
+              slivers: [
+                SliverAppBar(floating: true, flexibleSpace: CustomAppbar()),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                    return Column(
+                      children: [
+                        MoviesSlideshow(movies: moviesSlideshowProvider),
+                        MovieHorizontalListview(
+                          movies: nowPlayingMovies,
+                          title: "En cines",
+                          subtitle: "Sábado 27",
+                          loadNextPage: () {
+                            ref
+                                .read(nowPlayingMoviesProvider.notifier)
+                                .loadNextPage();
+                          },
+                        ),
+                        // Proximamente
+                        MovieHorizontalListview(
+                          movies: upComingMovies,
+                          title: "Proximamente",
+                          loadNextPage: () {
+                            ref
+                                .read(upComingMoviesProvider.notifier)
+                                .loadNextPage();
+                          },
+                        ),
+                        // Peliculas populares
+                        MovieHorizontalListview(
+                          movies: popularMovies,
+                          title: "Populares",
+                          subtitle: "En este mes",
+                          loadNextPage: () {
+                            ref
+                                .read(popularMoviesProvider.notifier)
+                                .loadNextPage();
+                          },
+                        ),
+                        MovieHorizontalListview(
+                          movies: topRatedMovies,
+                          title: "Mejor calificadas",
+                          subtitle: "Desde siempre",
+                          loadNextPage: () {
+                            ref
+                                .read(topRatedMoviesProvider.notifier)
+                                .loadNextPage();
+                          },
+                        ),
+                        SizedBox(height: 8.h),
+                      ],
+                    );
+                  }, childCount: 1),
+                ),
+              ],
+            ),
+        );
   }
 }
